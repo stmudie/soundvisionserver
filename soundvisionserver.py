d4e720d7fc73464b07db7d2d8492ed4ac0a14924
Stephen <stephen.mudie@synchrotron.org.au>
2014-03-16 23:12:29 -0600

    latest changes

--- soundvisionserver.py
+++ soundvisionserver.py
@@ -1,8 +1,16 @@
 from pyplayshow import pyplayshow
 from time import sleep
+from yamaha import yamaha
+
+y = yamaha(config='yamaha/config.yml')
+
+current = y.currentinput()
+y.changeinput('V-AUX')
 
 a = pyplayshow()
 
-a.show('test', time = 0)
-a.play('/mnt/shared/ding.wav')
-sleep(5)
+a.show('Scan Finished', colour=(200,100,200),background=(200,100,100),time = 0)
+sound = a.play('/root/ExampleNorm.ogg')
+sleep(sound.get_length())
+
+y.changeinput(current)
